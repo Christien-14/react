81bda3efa952cf695c8d5029be7876b0f5caeced
Christien Moses <christiankasereka00@gmail.com>
2024-05-29 13:35:32 +0200

    modification articleform.js et articles.js

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -32,7 +32,7 @@ const Articles = () => {
 
   return (
     <section className="p-8">
-      <h3 className="text-2xl font-bold text-center mb-4">Mes derniers articles</h3>
+      <h3 className="text-2xl font-bold text-center mb-4"> Articles</h3>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         {articles.map((article, index) => (
           <div key={index} className="border p-4">
