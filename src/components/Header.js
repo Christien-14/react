e7e74cf90b42e2549705d01ec931013015efb4b7
Christien Moses <christiankasereka00@gmail.com>
2024-10-22 13:15:22 +0200

    divers

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,7 +18,7 @@ const Header = () => {
       <h1>WEBSITE TEST ANIWHERE</h1>
       <div className="flex space-x-4">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-flex items-center">
-          <span>Nous écrire</span>
+          <span>MAIL</span>
         </button>
         <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded inline-flex items-center">
           <a href="https://github.com/" target="_blank" rel="noopener noreferrer">GitHub</a>
