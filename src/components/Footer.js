4f4eab9c455d2ed6afb7f93ea0c45f4eb8576450
Christien Moses <christiankasereka00@gmail.com>
2024-10-22 13:38:11 +0200

    divers

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -22,7 +22,7 @@ const Footer = () => {
       <a href="https://telegram.org/" target="_blank" rel="noopener noreferrer">
         <FaTelegram className="text-2xl" />
       </a>
-      <a href="https://www.linkedin.com/mwlite/feed/?trk=p_mwlite_feed_updates-primary_nav" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
         <FaLinkedin className="text-2xl" />
       </a>
     </footer>
