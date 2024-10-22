1d818e890b49afc4cc9818cf543210f672c79aa8
Christien Moses <christiankasereka00@gmail.com>
2024-10-22 13:01:01 +0200

    divers

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,3 +1,15 @@
+/**
+    * @description      : 
+    * @author           : Christien Moses
+    * @group            : 
+    * @created          : 22/10/2024 - 13:00:40
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 22/10/2024
+    * - Author          : Christien Moses
+    * - Modification    : 
+**/
 import React from 'react';
 import { FaRegSmile } from 'react-icons/fa';
 
@@ -5,7 +17,7 @@ const Welcome = () => {
   return (
     <section className="text-center p-8">
       <FaRegSmile className="text-6xl mx-auto mb-4" />
-      <h2 className="text-4xl font-bold">HEY, BIENVENUE SUR MA PAGE.</h2>
+      <h2 className="text-4xl font-bold">BIENVENUE</h2>
     </section>
   );
 };
